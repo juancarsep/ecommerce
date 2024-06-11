55cccdb30ed7bc51f0821fb5c8063e25b9a915a7
juancarsep <carballosepejuan@gmail.com>
2024-06-11 12:10:16 -0300

    proyectos creados y product model creado

---
+++ products-service/src/main/java/ar/com/jics/products_service/model/Product.java
@@ -0,0 +1,23 @@
+package ar.com.jics.products_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String brand;
+    private String price;
+}
---
+++ shoppingCart-service/src/test/java/ar/com/jics/shoppingCart_service/ShoppingCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.jics.shoppingCart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

fabb994c077136af4cef714d3aee419f7097fa8f
juancarsep <carballosepejuan@gmail.com>
2024-06-11 11:57:25 -0300

    eurekaSV implementado

---
+++ eurekaSV/src/test/java/ar/com/jics/eurekaSV/EurekaSvApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.jics.eurekaSV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
